5598da8fbd62ba99bc9e1911dc0fed098c3e0b56
Cuevi <davidcuevascarrasco10@gmail.com>
2026-03-16 10:54:20 +0100

    Proyecto vehiculo

---
+++ GestorVehiculos/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GestorVehiculos
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Vehiculo miAuto = new Vehiculo("Toyota", "Corolla");
+            Vehiculo miMoto = new Vehiculo("Yamaha", "R6");
+
+            miAuto.MostrarInfo();
+            miMoto.MostrarInfo();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ GestorVehiculos/Vehiculo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorVehiculos
+{
+    public class Vehiculo
+    {
+        private string marca;
+        private string modelo;
+
+        public Vehiculo(string marca, string modelo)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+        }
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"Vehiculo: {marca} {modelo}");
+        }
+
+    }
+}
